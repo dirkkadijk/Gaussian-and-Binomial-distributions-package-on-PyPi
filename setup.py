3ae61d9aa1ba49abe830c5a5fb331e5cffeb681a
Dirk Kadijk <dkadijk@me.com>
2020-08-13 17:57:16 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_dirkkadijk',
+      version='0.1',
+      author="Dirk Kadijk",
+      author_email="dkadijk@me.com",
+      description='Gaussian and Binomial distributions created by Dirk Kadijk',
+      packages=['distributions_dirkkadijk'],
+      zip_safe=False)
